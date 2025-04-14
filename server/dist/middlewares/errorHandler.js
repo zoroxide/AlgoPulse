bc012d497730a17614154df267f617bd236de7d1
Loay Mohamed <loay@loay.space>
2025-04-15 00:42:24 +0200

    deploy shit v3

--- server/dist/middlewares/errorHandler.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-module.exports = function (err, req, res, next) {
-  console.error(err.stack);
-  res.status(500).json({
-    message: "Something went wrong",
-    error: err.message
-  });
-};

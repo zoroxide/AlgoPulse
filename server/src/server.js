540c33ac6b56c17fde01f95fcf7a8a160ba54d15
Loay Mohamed <loay@loay.space>
2025-04-15 01:04:37 +0200

    deployment shit level 55555555

--- server/src/server.js
+++ server/src/server.js
@@ -14,7 +14,13 @@ require('dotenv').config();
 
 const app = express();
 
-app.use(cors({ origin: true, credentials: true }));
+
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    credentials: true
+}));
+
 app.use(express.json());
 app.use(bodyParser.json());
 
